9ceb5b66dc974e79f7c8619c3bb7d9c955ae0bf0
Rodrigo Lucena <rodrigo.lucena09@gmail.com>
2020-08-23 18:35:13 -0300

    atualização 23082020

--- Bando de dados/BD.py
+++ Bando de dados/BD.py
@@ -1,5 +1,28 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(host='localhost',user='root', password='')
+banco = mysql.connector.connect(host='localhost',user='root', password='', database='coop')
+cursor = banco.cursor()
+#cursor.execute("CREATE DATABASE coop")
 
-cnx.close()
+#cursor.execute("CREATE TABLE cadastros(login VARCHAR(20), senha VARCHAR(10), empresa VARCHAR(30), email VARCHAR(30))")
+
+
+#comando_SQL = "INSERT INTO cadastros (login, senha, empresa, email) VALUES (%s,%s,%s,%s)"
+#dados = ("recicla1","rec123","Mundo Recicla","recicla01@gmail.com")
+#dados = ("recicla2","rec124","Global Recicla","grecicla@gmail.com")
+#dados = ("ambientecoop","coop124","Cooperativa Ambiente","ambcoop@gmail.com")
+#cursor.execute(comando_SQL,dados)
+#banco.commit() # quando fizer alteração no banco de dados
+
+comando_SQL ="SELECT * FROM cadastros"
+cursor.execute(comando_SQL)
+valores_lidos=cursor.fetchall()
+print(valores_lidos[1][1]) # permite selecionar o item como se fosse uma matriz
+print(type(valores_lidos[1][1]))
+print(valores_lidos[1][1]=='rec124')
+
+''' No Mysql, 
+para ver os bancos de dados: "show databases;", 
+para ver a estrutura da tabela: "describe nome_tabela;", 
+para mostrar todos os dados: "select * from nome_tabela;",
+''' 
